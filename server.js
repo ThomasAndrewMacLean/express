283d6620f4571d921713b3323e6f7eeeac0172fd
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2018-02-28 09:05:48 +0100

    test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 // var cheerio = require('cheerio');
 const app = express();
 
-app.use(express.static('public'))
+// app.use(express.static('public'))
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(process.env.PORT || 8080, () => console.log('All is ok'))
