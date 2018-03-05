57fdf9ac28bcedf7f9d071ca1385482d3c6dbfe8
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2018-03-05 05:25:46 +0100

    cors

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 
-app.use(corse());
+// app.use(corse());
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Origin', process.env.ORIGIN || 'http://localhost:8082');
